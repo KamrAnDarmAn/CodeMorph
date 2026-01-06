1e015798e8685a4b580f4a0465ac3c4ca9f32deb
KamrAnDarmAn <KamrAnDarmAn72@gmail.com>
2026-01-06 14:29:05 +0430

    fix: sitemap.tx

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,42 +1,34 @@
 import type { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://code-morph-meta.vercel.app/",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: "https://code-morph-meta.vercel.app/about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://code-morph-meta.vercel.app/language",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://code-morph-meta.vercel.app/serialization",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://code-morph-meta.vercel.app/numbers-convertor",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
-    {
-      url: "https://code-morph-meta.vercel.app/ast",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.5,
-    },
+  const baseUrl = "https://code-morph-meta.vercel.app";
+
+  const routes: {
+    path: string;
+    priority: number;
+    changefreq:
+      | "monthly"
+      | "weekly"
+      | "always"
+      | "hourly"
+      | "daily"
+      | "yearly"
+      | "never";
+  }[] = [
+    { path: "/", priority: 1, changefreq: "monthly" },
+    { path: "/about", priority: 0.8, changefreq: "monthly" },
+    { path: "/language", priority: 0.5, changefreq: "weekly" },
+    { path: "/serialization", priority: 0.5, changefreq: "weekly" },
+    { path: "/numbers-convertor", priority: 0.5, changefreq: "weekly" },
+    { path: "/ast", priority: 0.5, changefreq: "weekly" },
   ];
+
+  return routes.map((r) => ({
+    url: `${baseUrl}${r.path}`,
+    lastModified: new Date().toISOString().split("T")[0], // YYYY-MM-DD
+    changeFrequency: r.changefreq,
+    priority: r.priority,
+  }));
 }
